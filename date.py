0ce725bc7cfba3e05e2a1eaede78ec7d5fafb278
Vidit Solanki <38397337+ViditSolanki@users.noreply.github.com>
2020-09-04 17:49:46 +0530

    print command

--- date.py
+++ date.py
@@ -1,11 +1,1 @@
-month = ['January','February','March','April','May','June','July','August','September','October','November','December']
-MM = []
-mm = []
-Month = []
-date = raw_input('Enter date (dd-mm-yyyy): ')
-if date[3:5]<10:
-    mm.append(date[4:5])
-    i = int(mm[0])
-    MM.append(month[i])
-    print MM
-    print mm
+print "Hello World"
